988b22c7fb27ce9310e6619f19fb4eb5fd09c554
Mouzzam Tahir <moazzamtahir81@gmail.com>
2020-03-29 13:13:45 +0500

    added UI components to first screen

--- FoodBroker/ContentView.swift
+++ FoodBroker/ContentView.swift
@@ -7,28 +7,60 @@
 //
 
 import SwiftUI
+import Combine
+import Foundation
 
 struct ContentView: View {
+    @State var textFieldText = ""
+    
     var body: some View {
         VStack {
+            Spacer()
+            
             Text("Find My")
                 .foregroundColor(.white)
                 .font(.largeTitle)
                 .fontWeight(.bold)
-                .padding(.trailing,230)
+                .padding(.trailing, 190)
             
             Text("Community")
-            .foregroundColor(.white)
-            .font(.largeTitle)
-            .fontWeight(.bold)
-            .padding(.trailing,180)
+                .foregroundColor(.white)
+                .font(.largeTitle)
+                .fontWeight(.bold)
+                .padding(.trailing, 130)
+                .padding(.bottom, 30)
             
             
+            TextField("Enter Zip Code", text: $textFieldText)
+                .font(Font.system(size: 28, weight: .light, design: .rounded))
+                .background(Color(UIColor.white))
+                .textFieldStyle(RoundedBorderTextFieldStyle())
+                .padding(.leading, 50)
+                .padding(.trailing, 50)
+                .cornerRadius(50)
+                .keyboardType(.decimalPad)
+            
+            Button("Search In My Area") {
                 
+            }.frame(width: 315, height: 50)
+                .background(Color("ButtonColor"))
+                .foregroundColor(.white)
+                .cornerRadius(30)
+            
+            Spacer()
+            
+            Button("Start Giving") {
+                
+            }.frame(width: 315, height: 50)
+                .background(Color("ButtonColor"))
+                .foregroundColor(.white)
+                .cornerRadius(30)
+                .padding(.bottom, 50)
+            
         }.background(Image("background")
-        .scaledToFill()
-        .edgesIgnoringSafeArea(.all)
-        .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
+            .scaledToFill()
+            .edgesIgnoringSafeArea(.all)
+            .frame(width: UIScreen.main.bounds.width, height: UIScreen.main.bounds.height))
     }
 }
 
